98e9e1a2d0905d1ef1041358a607d6b91fe8350c
sage-Flugel <teddylsm@naver.com>
2024-10-16 01:14:45 +0900

    Title: 전화번호 바꾸기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241016/전화번호 바꾸기/change-phone-number.c
+++ 241016/전화번호 바꾸기/change-phone-number.c
@@ -1,10 +1,19 @@
 #include <stdio.h>
 
 int main() {
+    /*010은 문자열로 했을 시
     int mid,end,trash;
     char start[]="010";
     scanf("%d-%d-%d",&trash,&mid,&end);
 
     printf("%s-%d-%d",start,end,mid);
+    */
+    int mid,end;
+    
+    scanf("010-%d-%d",&mid,&end);
+
+    printf("010-%d-%d",end,mid);
+    
+    
     return 0;
 }
