319eeb06d2973ef0be8863fe26e2e7f7cc30de50
sage-Flugel <teddylsm@naver.com>
2024-10-16 02:19:51 +0900

    Title: 합과 평균의 차, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 241016/합과 평균의 차/sub-of-average-and-sum.c
+++ 241016/합과 평균의 차/sub-of-average-and-sum.c
@@ -3,6 +3,6 @@
 int main() {
     int a,b,c;
     scanf("%d %d %d",&a,&b,&c);
-    printf("%d\n%d\n%d",a+b+c,(a+b+c)/3,a+b+c-(a+b+c)/2)
+    printf("%d\n%d\n%d",a+b+c,(a+b+c)/3,a+b+c-(a+b+c)/2);
     return 0;
 }
