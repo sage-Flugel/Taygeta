c4312fb30799c792ff632cc225867a00664f81c1
sage-Flugel <teddylsm@naver.com>
2024-10-15 23:06:03 +0900

    Title: 실수와 문자 받아 출력하기, Time: 15ms, Memory: 0MB, Status: Runtime Error - CodeTree

--- 241015/실수와 문자 받아 출력하기/print-number--and-letter.c
+++ 241015/실수와 문자 받아 출력하기/print-number--and-letter.c
@@ -3,7 +3,7 @@
 int main() {
     char c = 'C';
     double a =89.84336, b = 234.5678;
-    scanf("%c\n%lf\nlf",&c,&a,&b);
+    scanf("%c\n%lf\nlf",c,&a,&b);
     printf("%c\n%.2lf\n%.2lf");
 
     return 0;
