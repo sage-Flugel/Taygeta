0cefd4ad77a0b59e3dc4a0fc47e26cf51b687148
sage-Flugel <teddylsm@naver.com>
2024-10-15 23:06:00 +0900

    Title: 실수와 문자 받아 출력하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241015/실수와 문자 받아 출력하기/print-number--and-letter.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    char c = 'C';
+    double a =89.84336, b = 234.5678;
+    scanf("%c\n%lf\nlf",&c,&a,&b);
+    printf("%c\n%.2lf\n%.2lf");
+
+    return 0;
+}
