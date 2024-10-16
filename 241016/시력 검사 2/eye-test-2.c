1bb94e475d6b10716d5faf21a3b82ce9f9411fcd
sage-Flugel <teddylsm@naver.com>
2024-10-16 23:44:41 +0900

    Title: 시력 검사 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241016/시력 검사 2/eye-test-2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    float a;
+    scanf("%f",&a);
+
+    if(a >= 1.5)
+        printf("High");
+    else if(a >= 0.5)
+        printf("Middle");
+    else
+        printf("Low");
+    return 0;
+}
