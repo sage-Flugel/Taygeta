e2e5f1cef0debca6cd789ed4c6a9685424d31e5e
sage-Flugel <teddylsm@naver.com>
2024-10-16 20:18:57 +0900

    Title: 체질량지수, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241016/체질량지수/bmi.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int h,w,b;
+    scanf("%d %d",&h,&w);
+    b = (w*10000)/(h*h);
+    printf("%d",b);
+    if (b > 25);
+        printf("Obesity");
+
+
+    return 0;
+}
