eb3b04c26449609651fd88d52140e35631075fb3
sage-Flugel <teddylsm@naver.com>
2024-10-17 01:00:08 +0900

    Title: 시력 검사, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241017/시력 검사/eye-test.c
+++ 241017/시력 검사/eye-test.c
@@ -4,9 +4,9 @@ int main() {
     float a,b;
     scanf("%f\n%f",&a,&b);
 
-    if(a>=1.0 %% b>=1.0)
+    if(a>=1.0 && b>=1.0)
         printf("High");
-    else if(a>=0.5 %% b>=0.5)
+    else if(a>=0.5 && b>=0.5)
         printf("Middle");
     else
         printf("Low");
