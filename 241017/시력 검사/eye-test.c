a39d02e06cf0ee27fcca08168e327281e225f1a9
sage-Flugel <teddylsm@naver.com>
2024-10-17 00:59:52 +0900

    Title: 시력 검사, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 241017/시력 검사/eye-test.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    float a,b;
+    scanf("%f\n%f",&a,&b);
+
+    if(a>=1.0 %% b>=1.0)
+        printf("High");
+    else if(a>=0.5 %% b>=0.5)
+        printf("Middle");
+    else
+        printf("Low");
+    return 0;
+}
