8e5ef80d04af40d86ba50ed2f8ad4a3733241f45
sage-Flugel <teddylsm@naver.com>
2024-10-17 00:37:13 +0900

    Title: 3 또는 5의 배수, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241017/3 또는 5의 배수/multiples-of-3-or-5.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    int a;
+    scanf("%d",&a);
+
+    if(a%3==0)
+        printf("YES");
+    else
+        printf("NO");
+    if(a%5==0)
+        printf("YES");
+    else
+        printf("NO");
+
+
+
+    return 0;
+}
