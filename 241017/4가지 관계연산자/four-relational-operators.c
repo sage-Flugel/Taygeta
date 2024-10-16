2f5aeed2e15cdfadf3c0a8e2a83b181589c9ace3
sage-Flugel <teddylsm@naver.com>
2024-10-17 00:44:07 +0900

    Title: 4가지 관계연산자, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241017/4가지 관계연산자/four-relational-operators.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int a,b;
+    scanf("%d %d",&a,&b);
+
+    printf("%d\n",a>=b);
+    printf("%d\n",a>b);
+    printf("%d\n",b>=a);
+    printf("%d\n",b>a);
+    
+
+  
+
+
+
+    return 0;
+}
