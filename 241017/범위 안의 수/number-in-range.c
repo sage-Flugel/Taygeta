b7417d439f1bb56f1c71b64d895f397d1a4a6f1a
sage-Flugel <teddylsm@naver.com>
2024-10-17 00:57:24 +0900

    Title: 범위 안의 수, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 241017/범위 안의 수/number-in-range.c
+++ 241017/범위 안의 수/number-in-range.c
@@ -4,7 +4,10 @@ int main() {
     int a;
     scanf("%d",&a);
 
-    printf("%d",10 <= a && a <=20);
+    if (10 <= a && a <=20)
+        printf("yes");
+    else
+        printf("no")
 
     return 0;
 }
