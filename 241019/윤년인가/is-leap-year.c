9156dea5a0bbc4875f3f4801bc7ec5efe7366c2b
sage-Flugel <teddylsm@naver.com>
2024-10-19 03:30:12 +0900

    Title: 윤년인가, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 241019/윤년인가/is-leap-year.c
+++ 241019/윤년인가/is-leap-year.c
@@ -4,10 +4,8 @@ int main() {
     int a;
     scanf("%d",&a);
 
-    if(a % 4 == 0){
-        if (a % 100 == 0 && a % 400 != 0)
-            printf("true");
-    }
+    if((a % 4 == 0) || (a % 100 == 0 && a % 400 != 0))
+        printf("true");
     else
         printf("false");
         
