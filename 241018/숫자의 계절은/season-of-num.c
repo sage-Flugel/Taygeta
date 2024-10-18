32ff752680d6bfecb70532637420ca7d0763d69f
sage-Flugel <teddylsm@naver.com>
2024-10-18 21:33:40 +0900

    Title: 숫자의 계절은, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 241018/숫자의 계절은/season-of-num.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int a= 0;
+    scanf("%d",&a);
+    
+    if(a == 12 || a <= 2)
+        printf("Winter");
+    else if (a <= 5)
+        printf("Spring")
+    else if (a <= 8)
+        printf("Summer");
+    else
+        printf("Fall");
+
+
+    return 0;
+}
