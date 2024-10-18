6e0eba0cd0235e9b8bb1428a67b47bca41a66bd6
sage-Flugel <teddylsm@naver.com>
2024-10-18 21:19:29 +0900

    Title: 범위 밖의 수, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241018/범위 밖의 수/number-out-of-range.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    
+    int a= 0;
+    scanf("%d",&a);
+    if (a >10 || a > 20)
+        printf("yes");
+    else
+        printf("no");
+
+
+
+
+    return 0;
+}
