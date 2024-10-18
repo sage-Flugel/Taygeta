3584c1e345d2cf2f4ca3bb6aad99aa08af7f0c37
sage-Flugel <teddylsm@naver.com>
2024-10-18 22:24:01 +0900

    Title: 두 사람, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 241018/두 사람/two-person.c
+++ 241018/두 사람/two-person.c
@@ -4,6 +4,8 @@ int main() {
     int a1,b1;
     char a2 = 'a',b2 ='a';
 
+    scanf("%d %c\n%d %c",&a1,&b1,&a2,&b2);
+
     if(a1 >= 19 && a2 == 'M')
         printf("1");
     else if(b1 >= 19 && b2 == 'M')
