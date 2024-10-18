0a699e868dd5136ad6f3788bd04508f78452fc7b
sage-Flugel <teddylsm@naver.com>
2024-10-18 22:23:15 +0900

    Title: 두 사람, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241018/두 사람/two-person.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    int a1,b1;
+    char a2 = 'a',b2 ='a';
+
+    if(a1 >= 19 && a2 == 'M')
+        printf("1");
+    else if(b1 >= 19 && b2 == 'M')
+        printf("1");
+    else    
+        printf("%d",1>2);
+    
+
+    
+        
+
+    
+    
+
+
+    return 0;
+}
