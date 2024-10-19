eae18460711c8685ea53503570d402909c854b25
sage-Flugel <teddylsm@naver.com>
2024-10-19 19:22:43 +0900

    Title: 중앙값 구하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 241019/중앙값 구하기/find-the-median.c
+++ 241019/중앙값 구하기/find-the-median.c
@@ -6,16 +6,17 @@ int main() {
 
     if (a > b && c < a)
         mid = a;
-    if (a < b && c > a)
+    if (a < b && c < a)
         mid = a;
     if (b > a && b < c)
         mid = b;
-    if (b < a && b > c)
+    if (b < a && b < c)
         mid = b;
     if (c > a && c < b)
         mid = c;
-    if (c < a && c > b)
+    if (c < a && c < b)
         mid = c;
+
     printf("%d",mid);
 
     return 0;
