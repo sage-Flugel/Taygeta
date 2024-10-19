4f85d688730d79edb03a1b6f0685c4f3fb3722b4
sage-Flugel <teddylsm@naver.com>
2024-10-19 12:12:11 +0900

    Title: 윤년인가, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 241019/윤년인가/is-leap-year.c
+++ 241019/윤년인가/is-leap-year.c
@@ -3,20 +3,12 @@
 int main() {
     int a;
     scanf("%d",&a);
-//(a % 100 == 0 && a % 400 != 0)
-    if(a % 4 == 0){
-        if (a >= 100){
-            if ((a % 100 == 0 && a % 400 != 0) || a%4 == 0)
-                printf("true");
-            else
-                printf("false");
-        }
-        else
-            printf("true");
+
+    if (a % 4 != 0) || (a % 100 == 0 && a % 400 != 0){
+        printf("false");
     }
     else
-        printf("false");
-        
+        printf("true");
 
     return 0;
 }
