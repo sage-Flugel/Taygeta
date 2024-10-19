53b4f5d51a04b7f917d620cbd1335fdc866b7e3f
sage-Flugel <teddylsm@naver.com>
2024-10-19 15:04:27 +0900

    Title: 코로나 메뉴얼, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241019/코로나 메뉴얼/covid-manual.c
+++ 241019/코로나 메뉴얼/covid-manual.c
@@ -3,36 +3,29 @@
 int main() {
     //a1 증상 a2 열
     char a1,b1,c1;
-    int a2,b2,c2,k;
+    int a2,b2,c2,k=0;
     
-    scanf("%c %d",&a1,&a2);
+    scanf("%c %d\n%c %d\n%c %d",&a1,&a2,&b1,&b2,&c1,&c2);
     if(a1 == 'Y' && a2 >= 37)
         k += 2;
     else
         k+=0;
-    
-    scanf("%c %d",&b1,&b2);
+
     if(b1 == 'Y' && b2 >= 37)
         k += 2;
     else
         k+=0;
 
-    scanf("%c %d",&c1,&c2);
     if(c1 == 'Y' && c2 >= 37)
         k += 2;
     else
         k+=0;
 
+
     if (k >= 4)
         printf("E");
     else
         printf("N");
-    
-    
-    
-
-
-
 
     return 0;
 }
