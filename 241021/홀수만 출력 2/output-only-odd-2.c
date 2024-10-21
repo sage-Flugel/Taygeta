d5574b6e242ec2a6a8de2f39eb63da2723e2c66f
sage-Flugel <teddylsm@naver.com>
2024-10-21 10:56:02 +0900

    Title: 홀수만 출력 2, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 241021/홀수만 출력 2/output-only-odd-2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int b,a;
+    scanf("%d %d",&b,&a);
+
+    for(int i = b; i >= a;i--){
+        if (i % 2 ==1)
+            printf("%d "i);
+    }
+    return 0;
+}
