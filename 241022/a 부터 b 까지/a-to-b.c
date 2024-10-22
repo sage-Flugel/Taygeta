fd9d5f105bd8c497e9387dbc435db56be1941a3e
sage-Flugel <teddylsm@naver.com>
2024-10-22 23:37:35 +0900

    Title: a 부터 b 까지, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 241022/a 부터 b 까지/a-to-b.c
+++ 241022/a 부터 b 까지/a-to-b.c
@@ -3,15 +3,16 @@
 int main() {
     int a,b,i,k=0;
     scanf("%d %d",&a,&b);
-
+    
     for(i = a;a <= b;i++){
+        printf("%d ",a);
         if(i%2==1){
             a*=2;
         }
         else{
             a+=3;
         }
-        printf("%d ",a);
+        
     }
     return 0;
 }
