f547b64b8b684632574b8bba05d5a461fdefe3ea
sage-Flugel <teddylsm@naver.com>
2024-10-22 23:29:18 +0900

    Title: 100점 도달하기, Time: 1095ms, Memory: 51MB, Status: Time Limit Exceed - CodeTree

--- 241022/100점 도달하기/reach-100-point.c
+++ 241022/100점 도달하기/reach-100-point.c
@@ -4,17 +4,17 @@ int main() {
     int a,i;
     scanf("%d",&a);
     for(i = a; 1<=100;i++){
-        if(i > 90){
+        if(i > 90)
             printf("A ");
-            else if(i > 80)
-                printf("B ");
-            else if(i > 70)
-                printf("C ");    
-            else if(i > 60)
-                printf("D ");
-            else
-                printf("F ");        
-        }
+        else if(i > 80)
+            printf("B ");
+        else if(i > 70)
+            printf("C ");    
+        else if(i > 60)
+            printf("D ");
+        else
+            printf("F ");        
+        
     }
     return 0;
 }
