3b902b282b51997b41722022ff323b4cb2db68c1
sage-Flugel <teddylsm@naver.com>
2024-10-23 00:20:45 +0900

    Title: 배수의 개수 2, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 241023/배수의 개수 2/number-of-multipliers-2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int i,cnt,a;
+    for(i = 0; i<10;i++){
+        scanf("%d",&a)
+        if(i % a == 1)
+            cnt++;
+    }
+    printf("%d\n",cnt);
+    return 0;
+}
