b16d6933ed9c0f35960a6ca2953468db72571d15
sage-Flugel <teddylsm@naver.com>
2024-10-22 23:45:38 +0900

    Title: 369 게임, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 241022/369 게임/369-game.c
+++ 241022/369 게임/369-game.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 
 int main() {
-    int a,i;
+    int a,i,forten;
     scanf("%d",&a);
 
     for(i=1; i<= a;i++){
-        if(i % 3 == 0 || i % 60 == 0 || i % 90 == 0)
+        forten = i % 10;
+        if(i % 3 == 0 || i % 60 == 0 || i % 90 == 0 || forten % 3 == 0)
             printf("0 ");
         else
             printf("%d ",i);
