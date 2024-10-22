35585d6e73a5aaf792dce3da37760781766f0bfa
sage-Flugel <teddylsm@naver.com>
2024-10-22 23:35:44 +0900

    Title: a 부터 b 까지, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 241022/a 부터 b 까지/a-to-b.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int a,b,i,k=0;
+    scanf("%d %d",&a,&b);
+    for(i =a;i<=b;i++){
+        if(i%2==1){
+            k*=2
+        }
+        else{
+            k+=3;
+        }
+        printf("%d ",k);
+    }
+    return 0;
+}
