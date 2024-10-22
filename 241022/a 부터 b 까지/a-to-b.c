8a297a7e52c70d64b79762605436209c7bd1813a
sage-Flugel <teddylsm@naver.com>
2024-10-22 23:36:03 +0900

    Title: a 부터 b 까지, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 241022/a 부터 b 까지/a-to-b.c
+++ 241022/a 부터 b 까지/a-to-b.c
@@ -3,9 +3,10 @@
 int main() {
     int a,b,i,k=0;
     scanf("%d %d",&a,&b);
+
     for(i =a;i<=b;i++){
         if(i%2==1){
-            k*=2
+            k*=2;
         }
         else{
             k+=3;
