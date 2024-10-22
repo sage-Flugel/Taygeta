1fd726b9030719152a0a5f94c9520635533c6d3c
sage-Flugel <teddylsm@naver.com>
2024-10-22 23:28:23 +0900

    Title: 100점 도달하기, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 241022/100점 도달하기/reach-100-point.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int a,i;
+    scanf("%d",&a);
+    for(i = a; 1<=100;i++){
+        if(i > 90){}
+            printf("A ");
+            else if(i > 80)
+                printf("B ");
+            else if(i > 70)
+                printf("C ");    
+            else if(i > 60)
+                printf("D ");
+            else
+                printf("F ");        
+        }
+    }
+    return 0;
+}
