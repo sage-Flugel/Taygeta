28f8de7f471c14acd2a3dda8bd2987ea89dcb951
sage-Flugel <teddylsm@naver.com>
2024-10-22 17:53:47 +0900

    Title: 짝수만 출력 2, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 241022/짝수만 출력 2/output-only-even-2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int a,b;
+    scanf("%d %d",&a,&b);
+    while(a <= b){
+        if(b%2==0)
+            printf("%d ",n);
+        b--;
+    }
+    return 0;
+}
