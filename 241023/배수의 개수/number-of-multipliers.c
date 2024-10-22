48fed4a0d7dab5d968a28b743b3fb194be213f0e
sage-Flugel <teddylsm@naver.com>
2024-10-23 00:27:41 +0900

    Title: 배수의 개수, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 241023/배수의 개수/number-of-multipliers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int i,cnt1=0,cnt2=0;
+    for(i = 0; i< 10; i++){
+        scanf("%d",&a);
+        if(a % 3 == 0)
+            cnt1++;
+        else if(a % 5 == 0)
+            cnt2++;
+        }
+
+    printf("%d %d",cnt1,cnt2);
+    return 0;
+}
