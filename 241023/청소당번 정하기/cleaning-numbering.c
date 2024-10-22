4b8d786f28f4d4b59983313c2c208529f6c740fb
sage-Flugel <teddylsm@naver.com>
2024-10-23 00:45:22 +0900

    Title: 청소당번 정하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 241023/청소당번 정하기/cleaning-numbering.c
+++ 241023/청소당번 정하기/cleaning-numbering.c
@@ -18,7 +18,7 @@ int main() {
                     bath++;
                 }
                 else{
-                    bath++;
+                    hall++;
                 } 
             }
             else{
