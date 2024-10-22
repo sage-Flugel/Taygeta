33d4e926cf58bde68a74baa6b0c215edd3358656
sage-Flugel <teddylsm@naver.com>
2024-10-22 17:54:36 +0900

    Title: 짝수만 출력 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241022/짝수만 출력 2/output-only-even-2.c
+++ 241022/짝수만 출력 2/output-only-even-2.c
@@ -3,10 +3,10 @@
 int main() {
     int a,b;
     scanf("%d %d",&a,&b);
-    while(a <= b){
-        if(b%2==0)
-            printf("%d ",b);
-        b--;
+    while(b <= a){
+        if(a%2==0)
+            printf("%d ",a);
+        a--;
     }
     return 0;
 }
