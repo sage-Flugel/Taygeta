67a6c5d2fd96b2bc6d0b15a8de4aafa6e78bf91f
sage-Flugel <teddylsm@naver.com>
2024-10-22 23:44:13 +0900

    Title: 369 게임, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 241022/369 게임/369-game.c
+++ 241022/369 게임/369-game.c
@@ -5,7 +5,7 @@ int main() {
     scanf("%d",&a);
 
     for(i=1; i<= a;i++){
-        if(i % 3 == 0 || i % 60 != 0 || i % 90 != 0)
+        if(i % 3 == 0 || i % 60 == 0 || i % 90 == 0)
             printf("0 ");
         else
             printf("%d ",i);
