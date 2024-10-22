af02476bdd8c92f783571152f658c285e55b87bc
sage-Flugel <teddylsm@naver.com>
2024-10-23 00:44:09 +0900

    Title: 청소당번 정하기, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 241023/청소당번 정하기/cleaning-numbering.c
+++ 241023/청소당번 정하기/cleaning-numbering.c
@@ -4,32 +4,39 @@ int main() {
     int day,i,clas=0,bath=0,hall=0;
     scanf("%d",&day);
     /*
-    2일 교실
-    3일 복도
-    12일 화장실
+    2일 교실 clas
+    3일 복도 hall
+    12일 화장실 bath
     날짜가 겹치면 주기 긴걸로( ex)복도랑 화장실 겹치면 화장실 )
     */
 
     
     for(i = 0; i<day;i++){
         if(i%2==0){
-            if(i % 3 == 0)
-                hall++;
-
-            else if(i % 12 == 0)
-                bath++;
-            else
+            if(i%3==0){
+                if(i%12==0){
+                    bath++;
+                }
+                else{
+                    bath++;
+                } 
+            }
+            else{
                 clas++;
+            }
         }
-        else if(i%3==0){
-            if(i % 12 == 0)
+        if else(i%3==0){
+            if(i%12==0){
                 bath++;
-            else
+            }
+            else{
                 hall++;
+            }
         }
-        else if(i%12==0){
+        if else(i%12==0)
             bath++;
-        }
+
+        
     }
     printf("%d %d %d",clas,hall,bath);
     return 0;
