1b56182d18f1d03f1b618e13869dd23b5468f993
sage-Flugel <teddylsm@naver.com>
2024-10-22 21:05:57 +0900

    Title: 숫자의 증감, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241022/숫자의 증감/increasing-and-decreasing-numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    char c;
+    int n;
+    scanf("%c %d",&c,&n);
+
+    if(c == 'A'){
+        for(int i = 1; i <= n;i++){
+            printf("%d ",i);
+        }
+    }
+    else{
+        for(int i = n; i > 0; i--)
+             printf("%d ",i);
+    }
+        
+    
+
+    return 0;
+}
