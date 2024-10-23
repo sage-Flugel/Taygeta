ebd9b76f1c910272399361e77fe86b252eeca54a
sage-Flugel <teddylsm@naver.com>
2024-10-23 11:59:43 +0900

    Title: 윤년 횟수 구하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241023/윤년 횟수 구하기/number-of-leap-years.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+// i % 100 == 0 && i % 400 != 0 은 평년
+int main() {
+    int a,i,cnt=0;
+    scanf("%d",&a);
+    for(i = 1; i<=a;i++){
+        if(i%4==0)
+            cnt++;
+        else if(i % 100 == 0 && i % 400 != 0)
+            cnt = cnt;
+        
+
+    }
+    printf("%d",cnt-1);
+    return 0;
+}
