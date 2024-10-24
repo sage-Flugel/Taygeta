a99419a7fa256e20c00827b86ed92bac77fa6b7c
sage-Flugel <teddylsm@naver.com>
2024-10-25 02:26:25 +0900

    Title: 총합 구하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241025/총합 구하기/find-the-total.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int i,a,b,sum = 0;
+    scanf("%d %d",&a,&b);
+    for(i = a;i<=b; i++){
+        if(i % 6 == 0 && i % 8 != 0)
+            sum++;
+    }
+    printf("%d",sum);
+    return 0;
+}
