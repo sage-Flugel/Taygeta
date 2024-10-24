5df78922d2d6ce60bc03ca5e8136f280469776ba
sage-Flugel <teddylsm@naver.com>
2024-10-25 02:26:38 +0900

    Title: 총합 구하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241025/총합 구하기/find-the-total.c
+++ 241025/총합 구하기/find-the-total.c
@@ -5,7 +5,7 @@ int main() {
     scanf("%d %d",&a,&b);
     for(i = a;i<=b; i++){
         if(i % 6 == 0 && i % 8 != 0)
-            sum++;
+            sum+=i;;
     }
     printf("%d",sum);
     return 0;
