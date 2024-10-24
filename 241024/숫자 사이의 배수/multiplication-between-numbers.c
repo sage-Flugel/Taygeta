bef39eab85a4e46ce03dbc478cb35db36162b6b0
sage-Flugel <teddylsm@naver.com>
2024-10-24 11:59:08 +0900

    Title: 숫자 사이의 배수, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241024/숫자 사이의 배수/multiplication-between-numbers.c
+++ 241024/숫자 사이의 배수/multiplication-between-numbers.c
@@ -4,12 +4,13 @@ int main() {
     int a,b,i;
     double cnt=0,sum=0;
     scanf("%d %d",&a,&b);
-    for(i = a; i<=b;i++){
+
+    for(i = a; i <= b; i++){
         if(i % 5 == 0 || i % 7 == 0){
+            sum += i;
             cnt++;
-            sum+=i;
         }
     }
-    printf("%lf %.1lf",sum,sum/cnt);
+    printf("%.0lf %.1lf",sum,sum/cnt);
     return 0;
 }
