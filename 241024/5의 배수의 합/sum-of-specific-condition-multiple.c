8f00e76320a723f44afcbf4f39d49728683ee036
sage-Flugel <teddylsm@naver.com>
2024-10-24 12:04:00 +0900

    Title: 5의 배수의 합, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241024/5의 배수의 합/sum-of-specific-condition-multiple.c
+++ 241024/5의 배수의 합/sum-of-specific-condition-multiple.c
@@ -4,12 +4,12 @@ int main() {
     int A,B,i,a,b,sum=0;
     scanf("%d %d",&A,&B);
     if(A > B){
-        A = b;
-        B = a;
+        a = B;
+        b = A;
     }
     else{
-        A = a;
-        B = b;
+        a = A;
+        b = B;
     }
     for(i = a; i <= b; i++){
         if(i % 5==0)
