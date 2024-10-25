9cbccccc414e0f58e73b3a0ea368c837a49879dd
sage-Flugel <teddylsm@naver.com>
2024-10-25 14:27:56 +0900

    Title: a의 b승, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241025/a의 b승/a-to-the-power-of-b.c
+++ 241025/a의 b승/a-to-the-power-of-b.c
@@ -4,9 +4,13 @@ int main() {
     int a,b,i;
     scanf("%d %d",&a,&b);
     int sum = a;
-    for(i = 1; i < b; i++){
-        sum *=a;
+    if(b != 0){
+        for(i = 1; i < b; i++){
+            sum *=a;
+        }
     }
+    else
+        sum = 1;
     printf("%d",sum);
     return 0;
 }
