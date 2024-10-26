d82abebda9ef74c3007dc1cd4141f41f060339ad
sage-Flugel <teddylsm@naver.com>
2024-10-26 22:31:44 +0900

    Title: 특정 조건의 온전수 구하기, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 241026/특정 조건의 온전수 구하기/print-onjeonsu.c
+++ 241026/특정 조건의 온전수 구하기/print-onjeonsu.c
@@ -1,19 +1,17 @@
 #include <stdio.h>
 
 int main() {
-    int a,i,cnt =0,one;
+    int a,i;
     scanf("%d",&a);
 
     one = a % 10;
-    
 
-    for(i = 1;i <=a;i++){
-        if( i % 2== 0 && one == 5 && i % 3 == 0 && 9 % i != 0)
-            continue;
+    for(i=1;i<=a;i++){
+        if(i % 2== 0 || one != 5 || (i % 3== 0 && i % 9 != 0))
+            continue
         else
             printf("%d ",i);
-       
-    }
 
+    }
     return 0;
 }
