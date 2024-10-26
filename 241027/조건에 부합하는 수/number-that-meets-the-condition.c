a4f7eb0810197cc9f6c4912771e66548c0649a97
sage-Flugel <teddylsm@naver.com>
2024-10-27 00:25:04 +0900

    Title: 조건에 부합하는 수, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241027/조건에 부합하는 수/number-that-meets-the-condition.c
+++ 241027/조건에 부합하는 수/number-that-meets-the-condition.c
@@ -4,7 +4,7 @@ int main() {
     int a,i;
     scanf("%d",&a);
     for(i = 1; i<=a;i++){
-        if((i % 2 ==0 && i % 4 ==0)||((i / 8) % 2 == 0)||((i % 7) < 4))
+        if((i % 2 ==0 && i % 4 !=0)||((i / 8) % 2 == 0)||((i % 7) < 4))
             continue;
         else
             printf("%d ",i);
