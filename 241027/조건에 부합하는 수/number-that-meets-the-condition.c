53e9b99ebbcffb80914f8b94dbd51f8bb728e468
sage-Flugel <teddylsm@naver.com>
2024-10-27 00:03:07 +0900

    Title: 조건에 부합하는 수, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241027/조건에 부합하는 수/number-that-meets-the-condition.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int a,i;
+    scanf("%d",&a);
+    for(i = 1; i<=a;i++){
+        if((i % 2 ==0 && i % 4 ==0)||((i / 8) % 2 == 0)||((i % 7) < 4))
+            continue;
+        else
+            printf("%d ",i);
+    }
+    return 0;
+}
