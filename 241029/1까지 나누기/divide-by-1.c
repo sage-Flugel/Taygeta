743bc203997709110bda2685a7d1c4953348c038
sage-Flugel <teddylsm@naver.com>
2024-10-29 22:45:57 +0900

    Title: 1까지 나누기, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 241029/1까지 나누기/divide-by-1.c
+++ 241029/1까지 나누기/divide-by-1.c
@@ -3,12 +3,18 @@
 int main() {
     int a,i,cnt=0;
     scanf("%d",&a);
-    for(i = 0; i<=a;i++){
-        if( a / i >= 0){
+
+    for(i = 1; i<=a;i++){
+        if(a <= 1){
             break;
         }
-        cnt++;
+        else{
+            cnt++;
+            a /=i;
+            
+        }
+        
     }
-    printf("%d",cnt);
+    printf("%d",cnt+1);
     return 0;
 }
