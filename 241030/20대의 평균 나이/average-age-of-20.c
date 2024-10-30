3e9936a394d92691990dca5e3b5ed767a3deb365
sage-Flugel <teddylsm@naver.com>
2024-10-30 20:47:11 +0900

    Title: 20대의 평균 나이, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 241030/20대의 평균 나이/average-age-of-20.c
+++ 241030/20대의 평균 나이/average-age-of-20.c
@@ -7,9 +7,9 @@ int main() {
         if(20 <= a && a < 30){
             b+=a;
             p++;
+        }
         else
             break;
-        }
     }
     printf("%.2lf",b/p);
     return 0;
