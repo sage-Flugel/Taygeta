81c2edaa28fa61527b9e6a6208f132e78f6b8291
sage-Flugel <teddylsm@naver.com>
2024-10-30 20:32:50 +0900

    Title: 0 나올때까지 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241030/0 나올때까지 출력/print-until-zero-is-given.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int a,i,sum=0;
+    while(1){
+        scanf("%d",&a);
+        if(a == 0){
+            break;
+        }
+        printf("%d\n",a);
+    }
+    return 0;
+}
