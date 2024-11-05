1c3b3f8fa438ebff37739e027c697813be20506a
sage-Flugel <teddylsm@naver.com>
2024-11-06 00:43:43 +0900

    Title: 합성수 판별기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241106/합성수 판별기/factorization-discriminator.c
+++ 241106/합성수 판별기/factorization-discriminator.c
@@ -4,10 +4,12 @@ int main() {
     int a,i,k;
     scanf("%d",&a);
     for(i = 2; i<a;i++){
-        if(a % i == 0)
-            k = 0;
+        if(a % i == 0){
+            k = 1;
+            break;
+        }
         else
-            k=1;
+            k=0;
     }
     if(k == 1)
         printf("C");
