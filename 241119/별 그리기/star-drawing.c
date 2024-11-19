6570bb9908fab33b99f634394b01209b93de8d16
sage-Flugel <teddylsm@naver.com>
2024-11-19 12:31:29 +0900

    Title: 별 그리기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241119/별 그리기/star-drawing.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main() {
+    int a,i,j,k;
+    scanf("%d",&a);
+    k = a-1;
+
+    for(i=1;i<=a+2;i++){
+        if(i % 2 == 1){
+            for(j=k;j>0;j--){
+                printf(" ");
+
+            }
+            for(j=0;j<i;j++){
+                printf("*");
+            }
+            k--;
+            printf("\n");
+        }
+    }
+    k = 1;
+    for(i=a;i>0;i--){
+        if(i % 2 == 1){
+            for(j=0;j<k;j++){
+                printf(" ");
+
+            }
+            for(j=0;j<i;j++){
+                printf("*");
+            }
+            k++;
+            printf("\n");
+        }
+    }
+    return 0;
+}
