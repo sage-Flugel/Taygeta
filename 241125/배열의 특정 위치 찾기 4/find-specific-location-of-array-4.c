84537ceacb2402fef5e9748c039c4a10b754719a
sage-Flugel <teddylsm@naver.com>
2024-11-25 21:22:49 +0900

    Title: 배열의 특정 위치 찾기 4, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241125/배열의 특정 위치 찾기 4/find-specific-location-of-array-4.c
+++ 241125/배열의 특정 위치 찾기 4/find-specific-location-of-array-4.c
@@ -1,15 +1,18 @@
 #include <stdio.h>
 
 int main() {
-    int i;
+    int i,c=0;
     int a[10],sum=0,cnt=0;
 
     for(i=0;i<10;i++){
         scanf("%d ",&a[i]);
-        if(a[i]==0)
+        if(a[i]==0){
+            c=1;
             continue;
+        }
+
         else{
-            if(a[i]%2==0){
+            if(a[i]%2==0 && c != 1){
                 sum+=a[i];
                 cnt++;
             }    
