dff174dda279e9517fcac85462500d7970b79e25
sage-Flugel <teddylsm@naver.com>
2024-11-26 19:38:48 +0900

    Title: 배열의 특정 위치 찾기, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 241126/배열의 특정 위치 찾기/find-specific-location-fo-array.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    int i
+    double a[10] = { 0 },sum1=0,sum2=0,cnt=0;
+    for(i=0;i<10;i++){
+        scanf("%lf",&a[i]);
+
+        if(i % 2 == 0)
+            sum1+=a[i];
+
+        if(i % 3 == 0){
+            sum2+=a[i];
+            cnt++;
+            }
+    }
+    printf("%.0lf %.1lf",sum1,sum2/cnt);
+    return 0;
+}
