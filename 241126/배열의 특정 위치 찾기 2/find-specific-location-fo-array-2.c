6b039ab965f86c7f0f97a2ca8ba0d736d12fc286
sage-Flugel <teddylsm@naver.com>
2024-11-26 20:52:42 +0900

    Title: 배열의 특정 위치 찾기 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 241126/배열의 특정 위치 찾기 2/find-specific-location-fo-array-2.c
+++ 241126/배열의 특정 위치 찾기 2/find-specific-location-fo-array-2.c
@@ -6,12 +6,10 @@ int main() {
     for(i=0;i<10;i++){
         scanf("%d",&a[i]);
         if(a[i] % 2 == 0){
-            if(c1 < a[i])
-                c1 = a[i];
+                c1 += a[i];
         }
         if(a[i] % 2 == 1){
-            if(c2 < a[i])
-                c2 = a[i];
+                c2 += a[i];
         }
     }
     if(c2 > c1)
