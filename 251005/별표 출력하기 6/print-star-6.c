ff1ebafbe089fc88c96dd0ec5ab0b128ea706019
sage-Flugel <teddylsm@naver.com>
2025-10-05 23:31:26 +0900

    Title: 별표 출력하기 6, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 251005/별표 출력하기 6/print-star-6.c
+++ 251005/별표 출력하기 6/print-star-6.c
@@ -18,14 +18,10 @@ int main() {
 
     for(i=cnt-1;i>0;i--){
 
-        for(j=0;j<=k-2;j++){
-            if(n==2)
-                break;
-            else if(n==3&&k==2)
-                break;
-            printf("  ");
-            k--;
-            }
+        for(j = 1; j < i; j++) {
+        printf("  ");
+        }
+
 
         for(j=0;j<(n-i)*2+1;j++){
             printf("* ");
