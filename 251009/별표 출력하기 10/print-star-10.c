701655159aed8fc7a895ce1b94048f2dc2a11415
sage-Flugel <teddylsm@naver.com>
2025-10-09 15:52:37 +0900

    Title: 별표 출력하기 10, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251009/별표 출력하기 10/print-star-10.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() {
+    int a,i,j,cnt=1;
+    scanf("%d",&a);
+    int ucnt=a;
+//홀수증가 짝수감소
+    for(i=1;i<=a*2;i++){
+        if(i%2==1){
+            for(j=1;j<=cnt;j++){
+                printf("* ");
+            }
+            cnt++;
+        }
+        else{
+            for(j=ucnt;j>0;j--){
+                printf("* ");
+            }
+            ucnt--;
+        }
+
+        printf("\n");
+    }
+    
+	return 0;
+	
+}
+
