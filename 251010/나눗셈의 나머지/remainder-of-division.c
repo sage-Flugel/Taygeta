1f0e9ea26acce76af769ddf07a64db21c24188a0
sage-Flugel <teddylsm@naver.com>
2025-10-10 23:16:23 +0900

    Title: 나눗셈의 나머지, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 251010/나눗셈의 나머지/remainder-of-division.c
+++ 251010/나눗셈의 나머지/remainder-of-division.c
@@ -4,27 +4,43 @@ int main() {
     int a,b;
     scanf("%d %d",&a,&b);
 
-    int onecnt[9] = {0};
+    int onecnt[10] = {0};
+    int cnt = 0;
 
     for(int i = 0; i<12; i++){
         if(a % b == 0)
             onecnt[0]++;
-        
         else if(a % b == 1)
             onecnt[1]++;
-        
-        else if(a % b == 1)
-            onecnt[1]++;
-        
-        else if(a % b == 1)
-            onecnt[1]++;
+        else if(a % b == 2)
+            onecnt[2]++;
+        else if(a % b == 3)
+            onecnt[3]++;
+        else if(a % b == 4)
+            onecnt[4]++;
+        else if(a % b == 5)
+            onecnt[5]++;
+        else if(a % b == 6)
+            onecnt[6]++;
+        else if(a % b == 7)
+            onecnt[7]++;
+        else if(a % b == 8)
+            onecnt[8]++;
+        else if(a % b == 9)
+            onecnt[9]++;
 
-        else if(a % b == 1)
-            onecnt[1]++;
-            
+        a/=b;
+
+        if(a < b)
+            break;
     }
 
+    for(int i=0;i<10;i++){
+        if(onecnt[i] > 0)
+            cnt =cnt + onecnt[i]*onecnt[i];
+    }
 
+    printf("%d",cnt);
     return 0;
 }
 
