6366c03d50ba1dd30557e06fcf354872bef9133b
sage-Flugel <teddylsm@naver.com>
2025-10-11 14:20:04 +0900

    Title: 특정 위치의 문자, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 251011/특정 위치의 문자/char-in-specific-location.c
@@ -0,0 +1,28 @@
+
+#include <stdio.h>
+
+int main() {
+
+	char word[6] = { 'L', 'E', 'B', 'R', 'O' ,'S'};
+
+    char fnd;
+    scanf("%c",&fnd);
+
+	int idx = -1;
+
+	for (int i = 0; i < 5; i++) {
+		if (word[i] == fnd) {
+			idx = i;
+		}
+	}
+
+	if (idx == -1) {
+		printf("None");
+	}
+	else {
+		printf("%d", idx);
+	}
+
+	return 0;
+
+}
