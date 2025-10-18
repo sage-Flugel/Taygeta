3c541b0c9969a9f187559fbd750ebbb31454148c
sage-Flugel <teddylsm@naver.com>
2025-10-18 23:14:55 +0900

    Title: 두 개의 격자 비교하기, Time: 16ms, Memory: 0MB, Status: Runtime Error - Codetree

---
+++ 251018/두 개의 격자 비교하기/compare-two-grid.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+//같으면 0
+int main() {
+    int n,m,i,j;
+    scanf("%d %d",&n,&m);
+
+    int a[n][m];
+    int b[n][m];
+    int ans[n][m];
+
+    for(i=0;i<n;i++){
+        for(j=0;j<m;j++){
+            scanf(" %d",a[i][j]);
+        }
+    }
+
+    for(i=0;i<n;i++){
+        for(j=0;j<m;j++){
+            scanf(" %d",b[i][j]);
+        } 
+    }
+
+    for(i=0;i<n;i++){
+        for(j=0;j<m;j++){
+            if(a[i][j] == b[i][j])
+                ans[i][j] = 0;
+            else
+                ans[i][j] = 1;
+        } 
+    }
+
+    for(i=0;i<n;i++){
+        for(j=0;j<m;j++){
+            printf("%d ",ans[i][j]);
+        } 
+        printf("\n");
+    }
+
+    return 0;
+}
