f50568d7ada4298e1ea8c9c1b2c5e571bcfa8c36
sage-Flugel <teddylsm@naver.com>
2025-10-19 13:28:12 +0900

    Title: 파스칼의 삼각형, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251019/파스칼의 삼각형/pascals-triangle.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main() {
+    int n,i,j;
+    scanf("%d",&n);
+
+    int a[n][n];
+    
+    for(i = 0; i < n; i++){
+        for(j = 0; j < n; j++){
+            a[i][j] = 0;
+        }
+    }
+
+    for(i = 0; i < n; i++){
+        a[i][0] = 1;
+        a[i][i] = 1;
+    }
+
+    for(i=2;i<n;i++){
+        for(j=1;j<n;j++){
+            a[i][j] = a[i-1][j-1] + a[i-1][j];
+        }
+    }
+
+
+    for(i=0;i<n;i++){
+            for(j=0;j<=i;j++){
+                printf("%d ",a[i][j]);
+            }
+        printf("\n");
+    }
+
+    return 0;
+}
