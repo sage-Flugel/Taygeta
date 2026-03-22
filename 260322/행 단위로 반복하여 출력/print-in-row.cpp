13380f4ffa71e4fd76a9445f7b7673e8a3e670c8
sage-Flugel <teddylsm@naver.com>
2026-03-22 13:31:23 +0900

    Title: 행 단위로 반복하여 출력, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260322/행 단위로 반복하여 출력/print-in-row.cpp
@@ -0,0 +1,21 @@
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int i,j,n;
+    cin >> n;
+
+
+	for (i = 0; i < n; i++) {
+		for (j = 1; j <= n; j++) {
+			cout << j;
+		}
+		cout << "\n";
+	}
+
+	return 0;
+
+}
+
+
