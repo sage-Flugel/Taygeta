09f0e105ad12ae713ec71ba81a052ee93d75a2fd
sage-Flugel <teddylsm@naver.com>
2026-03-22 13:33:29 +0900

    Title: 열 단위로 반복하여 출력, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260322/열 단위로 반복하여 출력/print-in-column.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int i,j,n,cnt=1;
+	cin >> n;
+
+
+	for (i = 0; i < n; i++) {
+		for (j = 1; j < n+1;j++){
+			cout << cnt;
+			
+		}
+		cout << "\n";
+		cnt++;
+	}
+
+	return 0;
+
+}
+
+
