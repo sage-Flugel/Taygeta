328cbfc54a01a6616644bb903f8140f6ed774c43
sage-Flugel <teddylsm@naver.com>
2026-03-24 13:55:49 +0900

    Title: 짝수이면서 합이 5의 배수인 수, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260324/짝수이면서 합이 5의 배수인 수/an-even-number-with-a-multiple-of-5-in-the-sum.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int cnter(int a){
+    int ans;
+
+    if(a % 2 == 0 && (a/10)% 5 == 0 && (a%10)% 5)
+        ans = 1;
+    else
+        ans = 0;
+    
+    return(ans);
+}
+
+
+int n;
+
+int main() {
+    cin >> n;
+
+    if(cnter(n) == 1)
+        cout << "Yes";
+    else
+        cout << "No";
+
+    return 0;
+}
