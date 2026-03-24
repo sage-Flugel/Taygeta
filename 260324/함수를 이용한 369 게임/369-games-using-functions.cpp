b8ebf98d78bd3cf48681af71b61520fc4455a794
sage-Flugel <teddylsm@naver.com>
2026-03-24 23:26:02 +0900

    Title: 함수를 이용한 369 게임, Time: 18ms, Memory: 0MB, Status: Passed - Codetree

--- 260324/함수를 이용한 369 게임/369-games-using-functions.cpp
+++ 260324/함수를 이용한 369 게임/369-games-using-functions.cpp
@@ -10,19 +10,19 @@ int is_3_in_there(int x,int y){
 
     for(int i = x; i <= y; i++){
         s = to_string(i);
-
+        chkr = 0;
         for(char c : s){
             if(c == '3' || c == '6' || c == '9'){
                 ans++;
-                chkr++;
+                chkr=1;
+                break;
             }
         }
-    }
-    for(int i = x; i <= y; i++){
+
         if(i % 3 == 0 && chkr == 0)
             ans++;
     }
-    
+
     return ans;
 
 }
