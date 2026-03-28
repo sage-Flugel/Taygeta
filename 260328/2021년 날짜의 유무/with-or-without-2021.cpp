723719f41b592312d8f073df25266d9b0dafcd86
teddylsm <teddylsm@naver.com>
2026-03-28 22:46:54 +0900

    Title: 2021년 날짜의 유무, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260328/2021년 날짜의 유무/with-or-without-2021.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+
+using namespace std;
+
+bool clnder(int month,int day){
+    if(month > 12)
+        return false;
+    if(day > 32)
+        return false;
+    if(month == 2)
+        if(day > 28)
+            return false;
+    
+    if(month <= 7 && month % 2 == 1){
+        if(day > 32)
+            return false;
+    }
+    else if(month == 4 || month == 6){
+        if(day > 30)
+            return false;
+    }
+    if(month > 7 && month % 2 == 0){
+        if(day > 32)
+            return false;
+    }
+    else if(month == 9 || month == 11){
+        if(day > 30)
+            return false;
+    }
+
+    return true;
+        
+}
+
+int main() {
+    int M, D;
+    cin >> M >> D;
+
+    if(clnder(M,D))
+        cout << "Yes";
+    else
+        cout << "No";
+
+
+
+    return 0;
+}
