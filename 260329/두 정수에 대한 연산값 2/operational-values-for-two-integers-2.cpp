4195ed37ef0390d37df9c1d6cabec5ea21d0029a
teddylsm <teddylsm@naver.com>
2026-03-29 14:12:22 +0900

    Title: 두 정수에 대한 연산값 2, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260329/두 정수에 대한 연산값 2/operational-values-for-two-integers-2.cpp
+++ 260329/두 정수에 대한 연산값 2/operational-values-for-two-integers-2.cpp
@@ -2,33 +2,25 @@
 
 using namespace std;
 
-void cclater(int a, int b);
-void make_a_big(int &big,int &small);// 항상 a가 b보다 크도록 조정
-
+void cclter(int &a, int &b);
 
 int main() { 
     int a, b;
     cin >> a >> b;
 
-    make_a_big(a,b);
-    cclater(a,b);
+    cclter(a,b);
 
     cout << a << " " << b;
+
     return 0;
 }
-
-void make_a_big(int &big,int &small){
-    int tmp;
-    if(big > small) // a가 b보다 크면
-        NULL;
-    else{
-        tmp = big;
-        big = small;
-        small = tmp;
+void cclter(int &a, int &b){
+    if(a > b){
+        a*=2;
+        b+=10;
     }
-
-}
-void cclater(int &a, int &b){
-    a*=2;
-    b+=10;
+    else{
+        b*=2;
+        a+=10;
+    }
 }
